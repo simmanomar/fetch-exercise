801ab8abee1c412ed06f237e30cda5e9a449b0b3
simmanomar <omar.ssimman@gmail.com>
2026-03-02 13:03:43 +0100

    scroll button and media query

--- main.js
+++ main.js
@@ -22,3 +22,26 @@ fetch("https://picsum.photos/v2/list")
 })
 .catch(error => console.log("Error:", error));
 
+//button
+const button = document.createElement("button")
+button.textContent = '⇧';
+button.classList.add('scroll-to-top'); //adds class so you can style it in css
+
+document.body.appendChild(button); //adds button to page
+
+//scroll event
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    button.classList.add('show'); //add class in css that shows button when you scroll 200 from the top
+  } else {
+    button.classList.remove('show'); //if not, don't show it
+  }
+});
+
+button.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
